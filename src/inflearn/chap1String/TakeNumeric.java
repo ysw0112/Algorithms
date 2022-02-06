47bf207ea0a19c92234323e73a5b7e6f6f81beb1
양서우 <yangseowoo@wemakeprice.com>
2022-02-06 23:56:39 +0900

    알고리즘 추가

---
+++ src/inflearn/chap1String/TakeNumeric.java
@@ -0,0 +1,23 @@
+package inflearn.chap1String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TakeNumeric {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(solution(input));
+    }
+    public static final int solution(String input){
+        int answer = 0;
+        char[] inputChars = input.toCharArray();
+        for (int i = 0; i < inputChars.length; i++) {
+            if(Character.isDigit(inputChars[i])) answer = answer * 10 + Character.getNumericValue(inputChars[i]);
+        }
+        return answer;
+    }
+
+}
