0d3e76142c35fb352ab6ed13faa0736233c8cda6
양서우 <yangseowoo@wemakeprice.com>
2022-02-07 23:40:52 +0900

    알고리즘 추가

---
+++ src/inflearn/chap1String/ShortLength.java
@@ -0,0 +1,43 @@
+package inflearn.chap1String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class ShortLength {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        String inputValue = split[0];
+        String findString = split[1];
+        String collect = Arrays.stream(solution(inputValue, findString)).mapToObj(String::valueOf).collect(Collectors.joining(" "));
+        System.out.println(collect);
+    }
+
+    private static int[] solution(String inputValue, String findValue) {
+        int[] answer = new int[inputValue.length()];
+        String[] split = inputValue.split("");
+        int tmp = 200;
+        for (int i = 0; i < split.length; i++) {
+            if (split[i].equalsIgnoreCase(findValue)) {
+                tmp = 0;
+            } else {
+                tmp++;
+            }
+            answer[i] = tmp;
+        }
+        tmp = 200;
+        for (int i = split.length - 1; i >= 0; i--) {
+            if (split[i].equalsIgnoreCase(findValue)) {
+                tmp = 0;
+            } else {
+                tmp++;
+            }
+            answer[i] = Math.min(answer[i], tmp);
+        }
+        return answer;
+    }
+}
