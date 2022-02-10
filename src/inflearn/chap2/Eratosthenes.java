888ab00f88e9ec0e1f70062b78c79423d1279f3a
양서우 <yangseowoo@wemakeprice.com>
2022-02-10 23:52:38 +0900

    알고리즘

--- src/inflearn/chap2/Eratosthenes.java
+++ src/inflearn/chap2/Eratosthenes.java
@@ -4,18 +4,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//입력 20
+//출력 8
 public class Eratosthenes {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int input = Integer.valueOf(br.readLine()).intValue();
-        solution(input)
+        System.out.println(solution(input));
     }
 
     private static int solution(int input) {
         int cnt = 0;
-        for (int i = 0; i < input; i++) {
-
+        for (int i = 2; i < input; i++) {
+            if (i == 2 || i == 3) cnt ++;
+            else if(i%2 != 0 && i%3 != 0) cnt ++;
         }
         return cnt;
     }
