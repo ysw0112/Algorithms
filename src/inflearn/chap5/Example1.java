f5ab9953b5d9bf3ba43c448db3433741dcc6ae8d
양서우 <yangseowoo@wemakeprice.com>
2022-02-28 22:39:55 +0900

    알고리즘풀이

---
+++ src/inflearn/chap5/Example1.java
@@ -0,0 +1,37 @@
+package inflearn.chap5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Example1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        solution(input);
+    }
+
+    private static void solution(String input) {
+        Deque<Character> deque = new ArrayDeque<>();
+        char[] chars = input.toCharArray();
+        String answer = "YES";
+        for (int i = 0; i < chars.length; i++) {
+            char aChar = chars[i];
+            if(aChar == '(') {
+                deque.push(aChar);
+            }else if(aChar == ')'){
+                if(deque.isEmpty()) {
+                    answer = "NO";
+                    break;
+                }
+                deque.pop();
+            }
+        }
+        if(!deque.isEmpty()) answer = "NO";
+        System.out.println(answer);
+    }
+
+}
